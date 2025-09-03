1fb9e4ca1b050dfd72f609da216de2c7fbf912c3
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-03 17:14:34 +0530

    Show Featured Post

--- blogproject/views.py
+++ blogproject/views.py
@@ -3,7 +3,9 @@ from blog.models import Blog, Category
 
 def home(request):
     categories = Category.objects.all()
+    featured_blogs = Blog.objects.filter(is_featured=True)
     context = {
         'categories': categories,
+        'featured_blogs': featured_blogs
     }
     return render(request, 'home.html', context)
