fe2ea6070fb070d5e65ce4efade096c891d2fcb7
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-03 16:01:54 +0530

    Create Blog Model

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     ordering = ('name',)
 
-admin.site.register(Category, CategoryAdmin)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'author', 'status', 'is_featured', 'created_at', 'updated_at')
+    list_filter = ('status', 'is_featured', 'category', 'author')
+    ordering = ('-created_at',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin),
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -12,3 +13,28 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+)
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/%y/%m/%d', blank=True, null=True)
+    description = models.TextField(max_length=1000)
+    body = models.TextField(max_length=5000)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
+
+    def __str__(self):
+        return self.title
