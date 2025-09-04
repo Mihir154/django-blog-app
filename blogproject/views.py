37dc5c1bfd225ff87d84e1f115e430806cb922a5
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-04 17:44:22 +0530

    Show Simple Post

--- blogproject/views.py
+++ blogproject/views.py
@@ -3,9 +3,11 @@ from blog.models import Blog, Category
 
 def home(request):
     categories = Category.objects.all()
-    featured_blogs = Blog.objects.filter(is_featured=True)
+    featured_blogs = Blog.objects.filter(is_featured=True, status='published')
+    blogs = Blog.objects.filter(is_featured=False, status='published')
     context = {
         'categories': categories,
-        'featured_blogs': featured_blogs
+        'featured_blogs': featured_blogs,
+        'blogs': blogs,
     }
     return render(request, 'home.html', context)
