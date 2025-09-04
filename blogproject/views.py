fe00a269b8f69fa33481c93e3279996f4c1d983d
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-04 21:28:48 +0530

    Context Processors

--- blogproject/views.py
+++ blogproject/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from blog.models import Blog, Category
+from blog.models import Blog
 
 def home(request):
     blogs = Blog.objects.filter(status="published").order_by("-created_at")
     featured_blogs = blogs.filter(is_featured=True)[:5]
-    categories = Category.objects.all()
 
     context = {
         "blogs": blogs,
         "featured_blogs": featured_blogs,
-        "categories": categories,
     }
     return render(request, "home.html", context)
