18e7076e7fd9008f331ae8f166c5f594924b9b69
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-04 18:27:48 +0530

    Design Category Page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog, Category
 
-# Create your views here.
 def category_posts(request, category_id):
-    blogs = Blog.objects.filter(category_id=category_id, status='published')
+    # category = get_object_or_404(Category, id=category_id)
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return redirect('home')
+
+    blogs = Blog.objects.filter(category=category, status="published")
+    categories = Category.objects.all()  # for navbar links
+
     context = {
-        'blogs': blogs,
-        'category_id': category_id
+        "blogs": blogs,
+        "category": category,
+        "categories": categories,
     }
-    return render(request, 'category_posts.html', context)
+    return render(request, "category_posts.html", context)
