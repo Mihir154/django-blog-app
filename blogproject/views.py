85736f26a3bce08a131fa57ce572f3dfe4771cd7
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-04 21:15:53 +0530

    Create Base File

--- blogproject/views.py
+++ blogproject/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from blog.models import Blog, Category
 
 def home(request):
+    blogs = Blog.objects.filter(status="published").order_by("-created_at")
+    featured_blogs = blogs.filter(is_featured=True)[:5]
     categories = Category.objects.all()
-    featured_blogs = Blog.objects.filter(is_featured=True, status='published')
-    blogs = Blog.objects.filter(is_featured=False, status='published')
+
     context = {
-        'categories': categories,
-        'featured_blogs': featured_blogs,
-        'blogs': blogs,
+        "blogs": blogs,
+        "featured_blogs": featured_blogs,
+        "categories": categories,
     }
-    return render(request, 'home.html', context)
+    return render(request, "home.html", context)
