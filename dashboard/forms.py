9fef9992902247fca7983c560b24aa4f2e51b7de
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-06 17:29:17 +0530

    Add user management functionality with views, forms, and templates

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from blog.models import Category, Blog
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('title', 'category', 'image', 'description', 'body', 'status', 'is_featured')
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, permission_required
 from django.template.defaultfilters import slugify
 from blog.models import Category, Blog
-from .forms import CategoryForm, PostForm
+from .forms import CategoryForm, PostForm, UserForm, EditUserForm
 
 
 @login_required(login_url="login")
@@ -105,3 +106,44 @@ def delete_posts(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect("posts")
+
+@login_required(login_url='login')
+@permission_required('auth.view_user', raise_exception=True)
+def users(request):
+    users = User.objects.all()
+    return render(request, 'dashboard/users.html', {'users': users})
+
+@login_required(login_url='login')
+@permission_required('auth.add_user', raise_exception=True)
+def add_users(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            form.save_m2m()
+            return redirect('users')
+    else:
+        form = UserForm()
+    return render(request, 'dashboard/add_users.html', {'form': form})
+
+@login_required(login_url='login')
+@permission_required('auth.change_user', raise_exception=True)
+def edit_users(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    else:
+        form = EditUserForm(instance=user)
+    return render(request, 'dashboard/edit_users.html', {'form': form})
+
+@login_required(login_url='login')
+@permission_required('auth.delete_user', raise_exception=True)
+def delete_users(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
