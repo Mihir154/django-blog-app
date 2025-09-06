72d9066b8ca0ce60ca158ed1601ff5cda3cb40e0
Mihir Prajapati <mihirprajapati0105@gmail.com>
2025-09-06 11:53:50 +0530

    Add edit category functionality with corresponding view and template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from blog.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm
@@ -34,3 +34,19 @@ def add_categories(request):
         'form': form
     }
     return render(request, 'dashboard/add_categories.html', context)
+
+@login_required(login_url='login')
+def edit_categories(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    else:
+        form = CategoryForm(instance=category)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'dashboard/edit_categories.html', context)
